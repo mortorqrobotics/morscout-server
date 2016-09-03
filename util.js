7185d751ddb010f7c3b48d574af56b1dd1625369
thatarvinguy <zadeharvin@gmail.com>
2016-09-03 12:34:56 -0700

    Fix middleware

--- util.js
+++ util.js
@@ -78,7 +78,7 @@ function clearDataPoints(team, context, cb) {
 
 //Middleware (Do these work?)
 exports.requireAdmin = function(req, res, next) {
-    if (req.user && (req.user.current_team.position == "admin" || req.user.current_team.scoutCaptain == true)) next();
+    if (req.user && (req.user.position == "admin" || req.user.scoutCaptain == true)) next();
     else res.end("fail");
 }
 
